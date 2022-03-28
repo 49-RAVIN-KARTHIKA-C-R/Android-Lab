3c7caafdf08d9466c91c955f2b53e969dfc23cf8
ravin <ravinkarthikacr1998@gmail.com>
2022-03-28 16:06:42 +0530

    spinner

---
+++ spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+String [] country={"india","usa","uk"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner sp=findViewById(R.id.spinner);
+        sp.setOnItemSelectedListener(this);
+        ArrayAdapter aa=new ArrayAdapter(this, android.R.layout.simple_spinner_item,country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(aa);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
